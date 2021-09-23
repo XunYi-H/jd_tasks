2ae55ad813a414921da1abd4c367c591dd990569
yiyule10 <71067732+yiyule10@users.noreply.github.com>
2021-09-23 17:25:51 +0800

    粉丝互动

--- jd_wxFans.js
+++ jd_wxFans.js
@@ -1,22 +1,29 @@
 /**
-粉丝互动，没啥水
+粉丝互动，尽量自己设置定时，在0点和1点抽奖，白天基本没水
 修改温某的脚本，由于温某不干活，只能自己动手修改了
 注意：脚本会加购，脚本会加购，脚本会加购
 若发现脚本里没有的粉丝互动活动。欢迎反馈给我
-cron 34 6,18 * * * https://raw.githubusercontent.com/star261/jd/main/scripts/jd_fan.js
+cron 34 5,18 * * * https://raw.githubusercontent.com/star261/jd/main/scripts/jd_fan.js
 * */
 const $ = new Env('粉丝互动');
 const jdCookieNode = $.isNode() ? require('./jdCookie.js') : '';
 const notify = $.isNode() ? require('./sendNotify') : '';
 let cookiesArr = [];
 const activityList = [
-    {"actid":"4e259914ef4c41c0b1006a55bb2c370f","endTime":1630857599000},
-    {"actid":"959ddcb1701d43fbbbd6e5b60136489f","endTime":1630857600000},
-    {"actid":"6994d2900dfc4cfba9ab0b2c12b725e6","endTime":1630684799000},
-    {"actid":"6b7811e7b0a4438893f2ac9d5f53a8b8","endTime":1630857599000},
-    {"actid":"eff9c47393be446f9dd576e26d13dd9d","endTime":1631635200000},
-    {"actid":"d6fe4bd6a34e4eb9b498932122453890","endTime":1630548000000},
-    {"actid":"5622386323bb4a82a2ed4e0158f7c6a7","endTime":1631289599000}
+    {"actid": "5bb3f94bdbca4165ae2af0d85c8e66b2", "endTime": 1632931199000},
+    {"actid": "2aac05400cfb47c1b91cf8152f90b8e7", "endTime": 1632931200000},
+    {"actid": "f1f1d34436ec4909aa7903f64205e283", "endTime": 1633622400000},
+    {"actid": "7377aba024114ddfa25bad00948a8722", "endTime": 1632931200000},
+    {"actid": "b6bd7155cefe443aae1e5a74383008f2", "endTime": 1635695999000},
+    {"actid": "2bfa9b34365140f8a51293043ca2821f", "endTime": 1633017599000},
+    {"actid": "3ae867cd253f42b1992ae90598b7549a", "endTime": 1632931200000},
+    {"actid": "2d2280b86b394cc09d436feecb4e5d3b", "endTime": 1632931200000},
+    {"actid": "0b8f1d09788947669f75c4bcc4fde4ae", "endTime": 1633017599000},
+    {"actid": "bde48555ac8b41669f516f892f95e21c", "endTime": 1633017599000},
+    {"actid": "86eab05c547d4d28829d269e2a5c4f1e", "endTime": 1633017599000},
+    {"actid": "73086c4fed7746e1b911a776d2e8662e", "endTime": 1633017599000},
+    {"actid": "179e156d768240db84393751fc9c427b", "endTime": 1633017599000},
+    {"actid": "bed695cfc40941c0a641eba935f9601e", "endTime": 1633017599000},
 ]
 if ($.isNode()) {
     Object.keys(jdCookieNode).forEach((item) => {
@@ -41,8 +48,8 @@ if ($.isNode()) {
         $.oldcookie = cookiesArr[i];
         $.isLogin = true;
         $.nickName = '';
-        await TotalBean();
         $.UserName = decodeURIComponent($.cookie.match(/pt_pin=([^; ]+)(?=;?)/) && $.cookie.match(/pt_pin=([^; ]+)(?=;?)/)[1]);
+        await TotalBean();
         console.log(`\n*****开始【京东账号${$.index}】${$.nickName || $.UserName}*****\n`);
         if (!$.isLogin) {
             $.msg($.name, `【提示】cookie已失效`, `京东账号${$.index} ${$.nickName || $.UserName}\n请重新登录获取\nhttps://bean.m.jd.com/bean/signIndex.action`, {"open-url": "https://bean.m.jd.com/bean/signIndex.action"});
